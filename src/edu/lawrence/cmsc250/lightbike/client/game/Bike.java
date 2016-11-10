d326ad371ee96233e14119b2ca4abe2c52b3a173
thislooksfun <thislooksfun@repbot.org>
2016-11-10 17:08:16 -0600

    Added direction

--- src/edu/lawrence/cmsc250/lightbike/client/game/Bike.java
+++ src/edu/lawrence/cmsc250/lightbike/client/game/Bike.java
@@ -13,11 +13,13 @@ public class Bike
 	private final BikeColor color;
 	private final List<Point2D> path = new ArrayList<>();
 	private Point2D location;
+	private Direction direction;
 	
-	public Bike(Point2D startPos, BikeColor color)
+	public Bike(Point2D startPos, Direction direction, BikeColor color)
 	{
 		this.location = startPos;
 		this.path.add(startPos);
+		this.direction = direction;
 		this.color = color;
 	}
 	
---
+++ src/edu/lawrence/cmsc250/lightbike/client/game/physics/Direction.java
@@ -0,0 +1,12 @@
+package edu.lawrence.cmsc250.lightbike.client.game.physics;
+
+/**
+ * @author thislooksfun
+ */
+public enum Direction
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT;
+}
