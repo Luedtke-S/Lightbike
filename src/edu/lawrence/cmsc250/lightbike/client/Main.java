ba1c4e421ffa5cdbd224bb140a365a04daea7b67
thislooksfun <thislooksfun@repbot.org>
2016-11-11 16:41:15 -0600

    Renamed fxml file

--- src/edu/lawrence/cmsc250/lightbike/client/Main.java
+++ src/edu/lawrence/cmsc250/lightbike/client/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("graphics/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("graphics/arena.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
