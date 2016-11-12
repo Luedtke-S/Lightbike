aead933ea6be9a80695baebd56e189710c720bb9
thislooksfun <thislooksfun@repbot.org>
2016-11-12 11:48:45 -0600

    Fixed START_OFFSET calculation

--- src/edu/lawrence/cmsc250/lightbike/client/game/Constants.java
+++ src/edu/lawrence/cmsc250/lightbike/client/game/Constants.java
@@ -8,5 +8,5 @@ public class Constants
 	/** The size of the game grid */
 	public static final int GRID_SIZE = 60;
 	/** How far from the edges the bikes should start */
-	public static final int START_OFFSET = GRID_SIZE - 4;
+	public static final int START_OFFSET = (GRID_SIZE / 2) - 4;
 }
