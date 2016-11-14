47a7408fa4642778d8abe4653174ead83dd691f4
thislooksfun <thislooksfun@repbot.org>
2016-11-14 16:56:16 -0600

    Fixed ROOM_LIST crash

--- src/edu/lawrence/cmsc250/lightbike/client/networking/RoomListEvent.java
+++ src/edu/lawrence/cmsc250/lightbike/client/networking/RoomListEvent.java
@@ -9,14 +9,18 @@ public class RoomListEvent implements PacketEvent
 	
 	RoomListEvent(String parseFrom)
 	{
-		// parseFrom format:
-		// {room1ID}:{room1Occupants}:{room1ReadyList}:{room1Name},{room2ID}:{room2Occupants}:{room2ReadyList}:{room2Name},...,{roomNID}:{roomNOccupants}:{roomNReadyList}:{roomNName}
-		String[] list = parseFrom.split(",");
-		Room[] rs = new Room[list.length];
-		for (int i = 0; i < list.length; i++) {
-			String[] roomData = list[i].split(":", 4);
-			rs[i] = new Room(Integer.parseInt(roomData[0]), Integer.parseInt(roomData[1]), Integer.parseInt(roomData[2]), roomData[3]);
+		if (parseFrom.isEmpty())
+			rooms = new Room[0];
+		else {
+			// parseFrom format:
+			// {room1ID}:{room1Occupants}:{room1ReadyList}:{room1Name},{room2ID}:{room2Occupants}:{room2ReadyList}:{room2Name},...,{roomNID}:{roomNOccupants}:{roomNReadyList}:{roomNName}
+			String[] list = parseFrom.split(",");
+			Room[] rs = new Room[list.length];
+			for (int i = 0; i < list.length; i++) {
+				String[] roomData = list[i].split(":", 4);
+				rs[i] = new Room(Integer.parseInt(roomData[0]), Integer.parseInt(roomData[1]), Integer.parseInt(roomData[2]), roomData[3]);
+			}
+			rooms = rs;
 		}
-		rooms = rs;
 	}
 }
